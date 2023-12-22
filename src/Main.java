ae39d35178428d133d3eac13a943cba537ebc750
alex <jason33@yandex.ru>
2023-12-23 02:58:51 +0300

    zadacha

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[]array={10,21,30,17,22,79,6,97,8,54};
+        System.out.println("увеличиваем четные числа на 1");
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]%2==0) {
+                array[i] = array[i] + 1;
+            }
+            System.out.println( array[i]);
+        }
+        System.out.println("вывод с 3 по 7 число");
+        sumArray(array);
+
+        }
+        public static int sumArray(int []array){
+        for (int i=3;i<7;i++){
+            if(i<array.length);
+            System.out.println( array[i]);
+        }
+        return array.length;
+        }
+
+
+  }
+
+
+
+
+
+
+
+
+
+             
+
+
+
+
+
+
+
+
